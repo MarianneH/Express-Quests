123d02b82b07628a3be47e9572868bd61b23c7c3
MarianneH <marianne.helbig@googlemail.com>
2022-11-24 13:29:23 +0100

    quest 04bis

--- app.js
+++ app.js
@@ -12,15 +12,16 @@ app.get("/", welcome);
 
 const movieHandlers = require("./movieHandlers");
 const userHandlers = require("./userHandlers");
+const { validateMovie, validateUser } = require("./validators.js");
 
 app.get("/api/movies", movieHandlers.getMovies);
 app.get("/api/movies/:id", movieHandlers.getMovieById);
-app.post("/api/movies", movieHandlers.postMovie);
+app.post("/api/movies", validateMovie, movieHandlers.postMovie);
 app.put("/api/movies/:id", movieHandlers.updateMovie);
 
 app.get("/api/users", userHandlers.getUsers);
 app.get("/api/users/:id", userHandlers.getUserById);
-app.post("/api/users", userHandlers.postUser);
+app.post("/api/users", validateUser, userHandlers.postUser);
 app.put("/api/users/:id", userHandlers.updateUser);
 
 app.listen(port, (err) => {
