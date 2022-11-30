1f1e816714437abd8252bd8b7d8f0a8cfd4715c8
MarianneH <marianne.helbig@googlemail.com>
2022-11-30 12:49:55 +0100

    added deleteMovie method

--- app.js
+++ app.js
@@ -18,6 +18,7 @@ app.get("/api/movies", movieHandlers.getMovies);
 app.get("/api/movies/:id", movieHandlers.getMovieById);
 app.post("/api/movies", validateMovie, movieHandlers.postMovie);
 app.put("/api/movies/:id", movieHandlers.updateMovie);
+app.delete("/api/movies/:id", movieHandlers.deleteMovie);
 
 app.get("/api/users", userHandlers.getUsers);
 app.get("/api/users/:id", userHandlers.getUserById);
