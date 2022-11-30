c32f032ebee20c4120c14711f627a07c5c87c5f3
MarianneH <marianne.helbig@googlemail.com>
2022-11-30 13:53:31 +0100

    added mit precise get request

--- userHandlers.js
+++ userHandlers.js
@@ -1,11 +1,26 @@
 const database = require("./database"); //import database
 
 const getUsers = (req, res) => {
-  //query something from data base
+  let sql = "select * from users";
+  const sqlValues = [];
+
+  if (req.query.language != null) {
+    sql += " where language = ?";
+    sqlValues.push(req.query.language);
+
+    if (req.query.city != null) {
+      sql += " and city = ?";
+      sqlValues.push(req.query.city);
+    }
+  } else if (req.query.city != null) {
+    sql += " where city = ?";
+    sqlValues.push(req.query.city);
+  }
+
   database
-    .query("select * from users")
-    .then(([users]) => {
-      res.json(users);
+    .query(sql, sqlValues)
+    .then(([movies]) => {
+      res.json(movies);
     })
     .catch((err) => {
       console.error(err);
@@ -70,7 +85,6 @@ const updateUser = (req, res) => {
 
 const deleteUser = (req, res) => {
   const id = parseInt(req.params.id);
-
   database
     .query(`delete from users where id = ?`, [id])
     .then(([result]) => {
