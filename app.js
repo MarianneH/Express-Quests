b2aab7bea6f414822e1a9ab2f1d6f49323f8c287
MarianneH1 <marianne.helbig@googlemail.com>
2023-01-02 14:33:36 +0100

    added JWT authentication and wall

--- app.js
+++ app.js
@@ -3,35 +3,10 @@ const express = require("express");
 const app = express();
 app.use(express.json());
 const port = process.env.APP_PORT ?? 5000;
-const hashPassword = require("./auth");
 
 const welcome = (req, res) => {
   res.send("Welcome to my favourite movie list");
 };
-
-app.get("/", welcome);
-
-const movieHandlers = require("./movieHandlers");
-const userHandlers = require("./userHandlers");
-
-const { validateMovie, validateUser } = require("./validators.js");
-
-app.get("/api/movies", movieHandlers.getMovies);
-app.get("/api/movies/:id", movieHandlers.getMovieById);
-app.post("/api/movies", validateMovie, movieHandlers.postMovie);
-app.put("/api/movies/:id", movieHandlers.updateMovie);
-app.delete("/api/movies/:id", movieHandlers.deleteMovie);
-
-app.get("/api/users", userHandlers.getUsers);
-app.get("/api/users/:id", userHandlers.getUserById);
-app.post(
-  "/api/users",
-  validateUser,
-  hashPassword.hashPassword,
-  userHandlers.postUser
-);
-app.put("/api/users/:id", userHandlers.updateUser);
-app.delete("/api/users/:id", userHandlers.deleteUser);
 
 app.listen(port, (err) => {
   if (err) {
@@ -40,3 +15,31 @@ app.listen(port, (err) => {
     console.log(`Server is listening on ${port}`);
   }
 });
+
+app.get("/", welcome);
+
+const movieHandlers = require("./movieHandlers");
+const userHandlers = require("./userHandlers");
+const { hashPassword, verifyPassword, verifyToken } = require("./auth");
+const { validateMovie, validateUser } = require("./validators.js");
+
+//public routes
+app.get("/api/movies", movieHandlers.getMovies);
+app.get("/api/movies/:id", movieHandlers.getMovieById);
+app.get("/api/users", userHandlers.getUsers);
+app.get("/api/users/:id", userHandlers.getUserById);
+app.post("/api/users", validateUser, hashPassword, userHandlers.postUser);
+
+app.post(
+  "/api/login",
+  userHandlers.getUserByEmailWithPasswordAndPassToNext,
+  verifyPassword
+);
+
+//protected routed
+app.use(verifyToken); // authentication wall : verifyToken is activated for each route after this line
+app.post("/api/movies", validateMovie, movieHandlers.postMovie);
+app.put("/api/movies/:id", movieHandlers.updateMovie);
+app.delete("/api/movies/:id", movieHandlers.deleteMovie);
+app.put("/api/users/:id", userHandlers.updateUser);
+app.delete("/api/users/:id", userHandlers.deleteUser);
--- userHandlers.js
+++ userHandlers.js
@@ -45,6 +45,27 @@ const getUserById = (req, res) => {
       res.status(500).send("Error retrieving data from database");
     });
 };
+
+const getUserByEmailWithPasswordAndPassToNext = (req, res, next) => {
+  const { email } = req.body;
+
+  database
+    .query("select * from users where email = ?", [email])
+    .then(([users]) => {
+      if (users[0] != null) {
+        req.user = users[0];
+
+        next();
+      } else {
+        res.sendStatus(401);
+      }
+    })
+    .catch((err) => {
+      console.error(err);
+      res.status(500).send("Error retrieving data from database");
+    });
+};
+
 const postUser = (req, res) => {
   const { firstname, lastname, email, city, language, hashedPassword } =
     req.body;
@@ -107,4 +128,5 @@ module.exports = {
   postUser,
   updateUser,
   deleteUser,
+  getUserByEmailWithPasswordAndPassToNext,
 };
